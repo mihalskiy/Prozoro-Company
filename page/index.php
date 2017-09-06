1c5e50c37837f938ae7ca0d55c763cecaf24f99b
mihalskiy <mihalskiy.rv@gmail.com>
2017-09-06 20:04:03 +0300

    add some template boptstrap 4

--- page/index.php
+++ page/index.php
@@ -4,9 +4,43 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="/resource/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/resource/css/style.css">
+  <script src="/resource/js/jquery.min.js"></script>
+  <script src="/resource/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+  </style>
 </head>
 <body>
 
@@ -22,13 +56,34 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li class="active"><a href="#">Головна</a></li>
+        <li>
+        <a href="#">
+        <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>
+         Файли
+        </a>
+        </li>
+        <li>
+        <a href="#">
+        <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+         Таблиці
+        </a>
+        </li>
+        <li>
+        <a href="#">
+        <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+         Чат
+        </a>
+        </li>
       </ul>
+>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li>
+        <a href="#">
+        <span class="glyphicon glyphicon-bell" aria-hidden="true"></span>
+         Повідомлення
+        </a>
+        </li
       </ul>
     </div>
   </div>
